0dc9c2152e968a0e41498627d8e102e7c8aba722
hampug1 <98275225+hampug1@users.noreply.github.com>
2023-06-27 17:21:25 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup(){
+    canvas = createCanvas(640, 420)
+    canvas.center();
+}
+
+function page2(){
+    window.location = "bottles.html";
+}
+
+function page3(){
+    window.location = "bedroom.html";
+}
+
+function page4(){
+    window.location = " tv_and_ac.html";
+}
+
+function page5(){
+    window.location = "fruit_bowl.html";
+}
+
+function page6(){
+    window.location = "laptop_and_phone.html";
+}
+
